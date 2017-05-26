c81b809a36d7e00a9a43f01b30ddc2dd844222de
Linus Skucas <linus33@comcast.net>
2017-05-26 16:42:29 -0700

    starting to use oop in other places

--- stopwords/stopstrip.py
+++ stopwords/stopstrip.py
@@ -1,8 +1,8 @@
-""" Strip stop words from text"""
+""" Strip stop words from search string"""
 import re
-
+"""
 # String to strip of stopwords
-needshelp = str.lower("how do I make a variable in python?")
+needshelp = str.lower(input("Enter a search string: "))
 # make search string list
 query = set(re.sub("[^\w]", " ",  needshelp).split())
 print(query)
@@ -15,4 +15,25 @@ print("\n") # Make a space in between lines of code
 
 # find and recreate string
 goods = query.difference(stopwords)
-print(goods)
+readyGoods = ' '.join(goods)
+print(readyGoods)"""
+
+
+class stripStop():
+	"""Strip stopwords from search string"""
+	
+	def __init__(self, searchQuery, stop="stopwords.txt"):
+		"""Gather options for class"""
+		self.query = set(re.sub("[^\w]", " ",  searchQuery).split())
+		self.stopwords = open(stop, "r");set([line.rstrip('\n') for line in stop])
+		
+	def stripStop(self):
+		"""Rid set of stopwords and turn to string"""
+		goods = self.query.difference(stopwords)
+		readyGoods = ' '.join(goods)
+		#for debuging:
+		#print(readyGoods)
+		return readyGoods
+		
+		
+striped  = stripStop.stripStop("how do you make a variable in python?")
